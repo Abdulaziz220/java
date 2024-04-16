119957eeff5fccdd700d6a6d1508b7bcc3780440
Your Name <you@example.com>
2024-04-16 12:35:06 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let numberOne = prompt("Ismingizni kiriting")
+let test = prompt("Familiyangizni kiriting")
+let age = prompt ("yoshingizni kiriting")
+let Science = prompt ("yoqtirgan faningiz")
+let manzil = prompt ("yashash manzilingiz")
+
+
+console.log(numberOne);
+console.log(test);
+console.log(age);
+console.log(Science);
+console.log(manzil);
